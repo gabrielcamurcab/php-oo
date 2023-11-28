a20d4f3fdf6ba3e94331145e8077789e49010675
Gabriel Camurça <gabriel@brtech.dev>
2023-11-28 09:29:40 -0300

    Encapsulamento

--- debug/objetoContaBancaria.php
+++ debug/objetoContaBancaria.php
@@ -5,4 +5,6 @@ require __DIR__ . '/../vendor/autoload.php';
 use App\ContaBancaria;
 
 $contaBancaria = new ContaBancaria();
-var_dump($contaBancaria);
+//var_dump($contaBancaria);
+$contaBancaria->setBanco("Banco Inter");
+var_dump($contaBancaria->getBanco());
--- src/ContaBancaria.php
+++ src/ContaBancaria.php
@@ -1,7 +1,41 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
 class ContaBancaria {
+    private string $banco;
+    private string $nomeTitular = "Gabriel Camurça";
+    private string $numeroAgencia = "5346";
+    private string $numeroConta = "33359-3";
+    private float $saldo = 0;
+
+    public function exibirDadosPublicos(): array 
+    {
+        return [
+            'nomeTitular' => $this->nomeTitular,
+            'numeroConta' => $this->numeroConta,
+        ];
+    }
+
+    private function exibirDadosDaConta(): array 
+    {
+        return [
+            'banco' => $this->banco,
+            'nomeTitular' => $this->nomeTitular,
+            'numeroAgencia' => $this->numeroAgencia,
+            'numeroConta' => $this->numeroConta,
+            'saldo' => $this->saldo
+        ];
+    }
+
+    public function setBanco(string $banco): void {
+        $this->banco = $banco;
+    }
+
+    public function getBanco(): string {
+        return $this->banco;
+    }
 
 }
