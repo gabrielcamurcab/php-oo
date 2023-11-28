31091e5bc2116ec3193fa0aef706c0ead0728b4e
Gabriel Camurça <gabriel@brtech.dev>
2023-11-28 09:44:28 -0300

    Getters e Setters

--- src/ContaBancaria.php
+++ src/ContaBancaria.php
@@ -6,10 +6,10 @@ namespace App;
 
 class ContaBancaria {
     private string $banco;
-    private string $nomeTitular = "Gabriel Camurça";
-    private string $numeroAgencia = "5346";
-    private string $numeroConta = "33359-3";
-    private float $saldo = 0;
+    private string $nomeTitular;
+    private string $numeroAgencia;
+    private string $numeroConta;
+    private float $saldo;
 
     public function exibirDadosPublicos(): array 
     {
@@ -19,7 +19,7 @@ class ContaBancaria {
         ];
     }
 
-    private function exibirDadosDaConta(): array 
+    public function exibirDadosDaConta(): array 
     {
         return [
             'banco' => $this->banco,
@@ -30,12 +30,46 @@ class ContaBancaria {
         ];
     }
 
+    public function getBanco(): string {
+        return $this->banco;
+    }
+
     public function setBanco(string $banco): void {
         $this->banco = $banco;
     }
 
-    public function getBanco(): string {
-        return $this->banco;
+    public function getNomeTitular(): string {
+        return $this->nomeTitular;
     }
+
+    public function setNomeTitular(string $nomeTitular): void {
+        $this->nomeTitular = $nomeTitular;
+    }
+
+    public function getAgencia(): string {
+        return $this->numeroAgencia;
+    }
+
+    public function setAgencia(string $numeroAgencia): void {
+        $this->numeroAgencia = $numeroAgencia;
+    }
+
+    public function getConta(): string {
+        return $this->numeroAgencia;
+    }
+
+    public function setConta(string $numeroConta): void {
+        $this->numeroConta = $numeroConta;
+    }
+
+    public function getSaldo(): float {
+        return $this->saldo;
+    }
+
+    public function setSaldo(float $saldo): void {
+        $this->saldo = $saldo;
+    }
+
+    
 
 }
